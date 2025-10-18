3b8004d93954cd556fb408fd9dab177cbdbc3c86
Shamik Mandal <shamikmandal170@gmail.com>
2025-10-18 21:48:24 +0530

    jkhkj

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
-const socket = io("http://localhost:8080"); //backend URL
-// const socket = io("https://backend-jobsphere.onrender.com"); //backend URL
+// const socket = io("http://localhost:8080"); //backend URL
+const socket = io("https://backend-jobsphere.onrender.com"); //backend URL
 export default socket;
